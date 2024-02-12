18a3249ff9c04d8c551c91051827fd4f1cbc371c
Maxeemja <maxeemja.inc@gmail.com>
2024-02-12 18:02:05 +0200

    added Toast message with interceptor

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { ApiService } from './services/api.service';
 import { HomeComponent } from './components/home/home.component';
+import { HandleErrorsInterceptor } from './interceptors/handle-errors.interceptor';
 
 @Component({
   selector: 'app-root',
---
+++ frontend/src/app/interceptors/handle-errors.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpEvent,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HandleErrorService } from '../services/handle-error.service';
+
+@Injectable()
+export class HandleErrorsInterceptor implements HttpInterceptor {
+  constructor(private errorService: HandleErrorService) {}
+/* 
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log('app interceptor...');
+    
+    return next.handle(request);
+  } */
+
+  public intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return new Observable((observer) => {
+      next.handle(req).subscribe(
+        (res: HttpEvent<any>) => {
+          if (res instanceof HttpResponse) {
+            observer.next(res);
+          }
+        },
+        (err: HttpErrorResponse) => {
+          console.log(err);
+          this.errorService.handleError(err);
+        }
+      );
+    });
+  }
+}
