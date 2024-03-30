d241d751abd9c25fb2e8d4295cf71f7a3fdcea81
Maxeemja <maxeemja.inc@gmail.com>
2024-03-30 12:57:29 +0200

    task details modal window init

--- frontend/src/app/components/modalAssignmentDetails/modal-assignment-details.component.ts
+++ frontend/src/app/components/modalAssignmentDetails/modal-assignment-details.component.ts
@@ -7,12 +7,30 @@ import {
 } from '@angular/core';
 import { DIALOG_DATA, DialogRef } from '@angular/cdk/dialog';
 import { Assignment } from '../../interfaces/Assignment';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialog,
+  MAT_DIALOG_DATA,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+
 @Component({
   selector: 'modal-assignment-details',
   templateUrl: './modal-assignment-details.component.html',
   styleUrl: './modal-assignment-details.component.scss',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ModalAssignmentDetailsComponent implements OnInit {
